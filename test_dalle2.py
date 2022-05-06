263d8094ceb64f4e724da9110bcb47fb633f0868
goldiusleonard <57057496+goldiusleonard@users.noreply.github.com>
2022-05-06 05:56:21 +0000

    Add files via upload

---
+++ test_dalle2.py
@@ -0,0 +1,104 @@
+import torch
+from torchvision import transforms as T
+from pathlib import Path
+import os
+from tqdm import tqdm
+from dalle2_pytorch import DALLE2, DiffusionPriorNetwork, DiffusionPrior, Unet, Decoder, OpenAIClipAdapter
+from torchvision.datasets.coco import CocoCaptions
+
+# Change your input size here
+input_image_size = 256
+
+# Change your train image root path here
+test_img_path = "./val2014/"
+
+# Change your train annot json path here
+test_annot_path = "./coco_annotations/captions_val2014.json"
+
+# Change your device ("cpu" or "cuda")
+device = "cuda"
+
+# Change the model weight save path here (end with ".pth")
+dalle2_load_path = "./dalle2.pth"
+
+# Change the test result image save path (should be a directory or folder)
+test_img_save_path = "./result"
+
+if not os.path.exists(test_img_save_path):
+    os.makedirs(test_img_save_path)
+
+transform = T.Compose([
+    T.Lambda(lambda img: img.convert('RGB') if img.mode != 'RGB' else img),
+    T.Resize(input_image_size),
+    T.CenterCrop(input_image_size),
+    T.ToTensor()
+])
+
+test_data = CocoCaptions(
+    root=test_img_path,
+    annFile=test_annot_path,
+    transform=transform
+)
+
+# openai pretrained clip - defaults to ViT/B-32
+OpenAIClip = OpenAIClipAdapter()
+
+prior_network = DiffusionPriorNetwork(
+    dim = 512,
+    depth = 6,
+    dim_head = 64,
+    heads = 8
+)
+
+diffusion_prior = DiffusionPrior(
+    net = prior_network,
+    clip = OpenAIClip,
+    timesteps = 100,
+    cond_drop_prob = 0.2
+).to(device)
+
+unet1 = Unet(
+    dim = 128,
+    image_embed_dim = 512,
+    cond_dim = 128,
+    channels = 3,
+    dim_mults=(1, 2, 4, 8)
+)
+
+unet2 = Unet(
+    dim = 16,
+    image_embed_dim = 256,
+    cond_dim = 128,
+    channels = 3,
+    dim_mults = (1, 2, 4, 8, 16)
+)
+
+decoder = Decoder(
+    unet = (unet1, unet2),
+    image_sizes = (128, 256),
+    clip = OpenAIClip,
+    timesteps = 100,
+    image_cond_drop_prob = 0.1,
+    text_cond_drop_prob = 0.5,
+    condition_on_text_encodings = True  # set this to True if you wish to condition on text during training and sampling
+).to(device)
+
+dalle2 = DALLE2(
+    prior = diffusion_prior,
+    decoder = decoder
+).to(device)
+
+dalle2.load_state_dict(torch.load(dalle2_load_path))
+
+for data in test_data:
+    _, target = data
+
+    test_img_tensors = dalle2(
+        target,
+        cond_scale = 2., # classifier free guidance strength (> 1 would strengthen the condition)
+    )
+
+    for test_idx, test_img_tensor in enumerate(test_img_tensors):
+        test_img = T.ToPILImage()(test_img_tensor)
+        test_save_path = os.path.join(test_img_save_path, f"{target[test_idx]}.jpg")
+        test_img.save(Path(test_save_path))
--- train_dalle2.py
+++ train_dalle2.py
@@ -1,8 +1,6 @@
 import torch
-import torch.nn as nn
 from torch.optim.lr_scheduler import ExponentialLR
 from torchvision import transforms as T
-from PIL import Image
 from pathlib import Path
 import os
 from tqdm import tqdm
@@ -10,7 +8,6 @@ from dalle2_pytorch import DALLE2, DiffusionPriorNetwork, DiffusionPrior, Unet, 
 from dalle2_pytorch.tokenizer import SimpleTokenizer
 from dalle2_pytorch.optimizer import get_optimizer
 from torchvision.datasets.coco import CocoCaptions
-from IPython.display import clear_output
 
 # Change your input size here
 input_image_size = 256
@@ -30,11 +27,20 @@ train_annot_path = "./coco_annotations/captions_train2014.json"
 # Change your device ("cpu" or "cuda")
 device = "cuda"
 
-# Change your diffusion prior model save path here
+# Change your diffusion prior model save path here (end with ".pth")
 diff_save_path = "./diff_prior.pth"
 
-# Change your diffusion prior model save path here
+# Change your diffusion prior model save path here (end with ".pth")
 decoder_save_path = "./decoder.pth"
+
+# Change the model weight save path here (end with ".pth")
+dalle2_save_path = "./dalle2.pth"
+
+# Change the test result image save path (should be a directory or folder)
+test_img_save_path = "./result"
+
+if not os.path.exists(test_img_save_path):
+    os.makedirs(test_img_save_path)
 
 transform = T.Compose([
     T.Lambda(lambda img: img.convert('RGB') if img.mode != 'RGB' else img),
@@ -66,8 +72,6 @@ diffusion_prior = DiffusionPrior(
     cond_drop_prob = 0.2
 ).to(device)
 
-# diffusion_prior = nn.DataParallel(diffusion_prior)
-
 unet = Unet(
     dim = 128,
     image_embed_dim = 512,
@@ -86,8 +90,6 @@ decoder = Decoder(
     text_cond_drop_prob = 0.5,
     condition_on_text_encodings=True
 ).to(device)
-
-# decoder = nn.DataParallel(decoder)
 
 if os.path.exists(diff_save_path):
     diffusion_prior.load_state_dict(torch.load(diff_save_path))
@@ -140,6 +142,70 @@ for curr_epoch in range(epoch):
             sched.step()
 
         if batch_idx % 1000 == 0:
-            print(f"loss: {loss.data[0]}")
+            print(f"loss: {loss.data}")
 
 torch.save(diffusion_prior.state_dict(), diff_save_path)
+
+opt = get_optimizer(decoder.parameters())
+sched = ExponentialLR(opt, gamma=0.01)
+
+for curr_epoch in range(epoch):
+    print("Run training decoder ...")
+    print(f"Epoch {curr_epoch+1} / {epoch}")
+    
+    for batch_idx in tqdm(idx_list):
+        if (batch_idx + batch_size) > train_size - 1:
+            iter_idx = range(batch_idx, train_size, 1)
+        else:
+            iter_idx = range(batch_idx, batch_idx+batch_size, 1)
+
+        image_list = []
+        text_list = []
+        
+        for curr_idx in iter_idx:
+            image, target = train_data[curr_idx]
+            image = image.unsqueeze(0).to(device)
+
+            text = tokenizer.tokenize(target).to(device)
+
+            text_size = len(text)
+            for i in range(text_size):
+                image_list.append(image)
+            
+            text_list.append(text)
+            
+        text = torch.cat(text_list, dim=0).to(device)
+        image = torch.cat(image_list, dim=0).to(device)
+
+        loss = decoder(image, text) # this can optionally be decoder(images, text) if you wish to condition on the text encodings as well, though it was hinted in the paper it didn't do much
+        opt.zero_grad()
+        loss.backward()
+        opt.step()
+
+        if batch_idx != 0 and batch_idx % 100 == 0:
+            torch.save(decoder.state_dict(), decoder_save_path)
+            sched.step()
+        
+        if batch_idx % 1000 == 0:
+            print(f"loss: {loss.data}")
+
+torch.save(decoder.state_dict(), decoder_save_path)
+
+dalle2 = DALLE2(
+    prior = diffusion_prior,
+    decoder = decoder
+).to(device)
+
+torch.save(dalle2.state_dict(), dalle2_save_path)
+
+test_input = ['Closeup of bins of food that include broccoli and bread.'] # text input for the model (can be more than one)
+
+test_img_tensors = dalle2(
+    test_input,
+    cond_scale = 2., # classifier free guidance strength (> 1 would strengthen the condition)
+)
+
+for test_idx, test_img_tensor in enumerate(test_img_tensors):
+    test_img = T.ToPILImage()(test_img_tensor)
+    test_save_path = os.path.join(test_img_save_path, f"{test_input[test_idx]}.jpg")
+    test_img.save(Path(test_save_path))
